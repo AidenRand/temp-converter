0ab87649774e9143dc0c55b26a4db754831b10f1
Cortrax <aidenrphotos@gmail.com>
2023-02-21 10:23:29 -0500

    Change converter class

--- main.py
+++ main.py
@@ -2,30 +2,36 @@ from tkinter import *
 from tkinter import font
 
 root = Tk()
-root.geometry('200x250')
+root.geometry("200x250")
 
-inputFont = font.Font(size=36)
-userInput = Entry(root, width=3, font=inputFont)
-userInput.place(x=55, y=20)
+userInput = Entry(root, width=50)
+userInput.place(x=50, y=10)
 
 class Convert:
-    def __init__(self, cTemp, fTemp):
-        self.cTemp = cTemp
-        self.fTemp = fTemp
+    def __init__(self, temp):
+        self.temp = int(temp)
         
     def celcius(self):
-        cTemp = (userInput.get() * 9/5) + 32
+        cTemp = (self.temp * 9/5) + 32
         return cTemp
     
     def fahrenheit(self):
-        fTemp = (userInput.get() - 32) * 5/9
+        fTemp = (self.temp - 32) * 5/9
         return fTemp
 
-cBtn = Button(root, text='CELCIUS', fg='#4A4A4A', command=celcius())
+def create_celc():
+    conv = Convert()
+    conv.celcius(userInput.get())
+
+
+def create_fahrenheit():
+    conv = Convert(userInput.get())
+    conv.fahrenheit()
+
+cBtn = Button(root, text="CELCIUS", fg="#4A4A4A", command=create_celc)
 cBtn.place(x=20, y=100)
 
-fBtn = Button(root, text='FAHRENHEIT', fg='#4A4A4A', command=fahrenheit())
+fBtn = Button(root, text="FAHRENHEIT", fg="#4A4A4A", command=create_fahrenheit)
 fBtn.place(x=100, y=100)
-
 
 root.mainloop()
