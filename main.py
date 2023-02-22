b983be387be64f93777bcdcedec38490d5817d9d
Cortrax <aidenrphotos@gmail.com>
2023-02-22 10:24:10 -0500

    Clean code

--- main.py
+++ main.py
@@ -2,30 +2,41 @@ from tkinter import *
 from tkinter import font
 
 root = Tk()
-root.geometry("200x250")
+root.geometry("380x155")
+root.title("Temp converter")
+root.config(bg="#EAEAEA")
 
 inputFont = font.Font(size=16)
 userInput = Entry(root, width=5, font=inputFont)
-userInput.place(x=50, y=10)
+userInput.place(x=60, y=10)
+
 
 class Convert:
     def __init__(self, temp):
         self.temp = int(temp)
-        
+
     def celcius(self):
-        cTemp = (self.temp * 9/5) + 32
+        cTemp = (self.temp * 9 / 5) + 32
         return cTemp
-    
+
     def fahrenheit(self):
-        fTemp = (self.temp - 32) * 5/9
+        fTemp = (self.temp - 32) * 5 / 9
         return fTemp
+
 
 def create_celc():
     conv = Convert(userInput.get())
     conv.celcius()
     tempFont = font.Font(size=26)
-    outTemp = Label(root, text=round(conv.fahrenheit(), 1), width=10, height=3, font=tempFont)
-    outTemp.place(x=0, y=130)
+    outTemp = Label(
+        root,
+        text=(round(conv.fahrenheit(), 1), "°"),
+        width=8,
+        height=2,
+        font=tempFont,
+        bg="#EAEAEA",
+    )
+    outTemp.place(x=190, y=40)
     userInput.delete(0, END)
 
 
@@ -33,14 +44,26 @@ def create_fahrenheit():
     conv = Convert(userInput.get())
     conv.fahrenheit()
     tempFont = font.Font(size=26)
-    outTemp = Label(root, text=round(conv.celcius(), 1),width=10, height=3, font=tempFont)
-    outTemp.place(x=0, y=130)
+    outTemp = Label(
+        root,
+        text=(round(conv.celcius(), 1), "°"),
+        width=8,
+        height=2,
+        font=tempFont,
+        bg="#EAEAEA",
+    )
+    outTemp.place(x=190, y=40)
     userInput.delete(0, END)
 
-cBtn = Button(root, text="CELCIUS", fg="#4A4A4A", command=create_celc)
-cBtn.place(x=20, y=100)
 
-fBtn = Button(root, text="FAHRENHEIT", fg="#4A4A4A", command=create_fahrenheit)
-fBtn.place(x=100, y=100)
+btnFont = font.Font(size=16)
+
+cBtn = Button(root, text="CELCIUS", fg="#2D2D2D", bg='#FAFAFA', font=btnFont, command=create_celc)
+cBtn.place(x=39, y=50)
+
+fBtn = Button(
+    root, text="FAHRENHEIT", fg="#2D2D2D", bg='#FAFAFA', font=btnFont, command=create_fahrenheit
+)
+fBtn.place(x=20, y=100)
 
 root.mainloop()
